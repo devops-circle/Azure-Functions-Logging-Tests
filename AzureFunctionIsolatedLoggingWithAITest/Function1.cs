f5c4c4bf1e0d9da383c4ed2a41998ef506489a9d
Ralph Jansen <ralph.jansen@outlook.com>
2022-08-30 10:02:00 +0200

    Create new project for logging WITH AI

---
+++ AzureFunctionIsolatedLoggingWithAITest/Function1.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace AzureFunctionIsolatedLoggingWithAITest
+{
+    public class Function1
+    {
+        private readonly ILogger _logger;
+
+        public Function1(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<Function1>();
+        }
+
+        [Function("Function1")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            _logger.LogInformation("Some logging test right now:");
+            _logger.LogTrace("Trace");
+            _logger.LogDebug("Debug");
+            _logger.LogInformation("Information");
+            _logger.LogWarning("Warning");
+            _logger.LogError("Error");
+            _logger.LogCritical("Critical");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString("Welcome to Azure Functions!");
+
+            return response;
+        }
+    }
+}
