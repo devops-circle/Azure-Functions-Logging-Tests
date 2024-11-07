d765f5e5744392bb3ead5b98d1764ba7346785ef
Ralph Jansen <ralph.jansen@outlook.com>
2024-11-07 16:12:52 +0100

    Remove default filter for application insights. See https://learn.microsoft.com/en-us/azure/azure-functions/dotnet-isolated-process-guide?tabs=hostbuilder%2Cwindows#managing-log-levels

--- Func.Isolated.Net8.With.AI/Program.cs
+++ Func.Isolated.Net8.With.AI/Program.cs
@@ -2,6 +2,7 @@ using Func.Isolated.Net8.With.AI;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
@@ -12,6 +13,23 @@ var host = new HostBuilder()
 
         // Setup DI
         services.AddTransient<IUserDataService, UserDataService>();
+    })
+    .ConfigureLogging(logging =>
+    {
+        // By default, logs with LogLevel.Warning or higher are sent to Application Insights.
+        // To change this, remove the default rule so other log levels are sent to Application Insights.
+        // See for more information: https://learn.microsoft.com/en-us/azure/azure-functions/dotnet-isolated-process-guide?tabs=hostbuilder%2Cwindows#managing-log-levels
+        // The default log level for Azure Functions is Information. So by removing the default rule, Information and above will be sent to Application Insights.
+        logging.Services.Configure<LoggerFilterOptions>(options =>
+        {
+            LoggerFilterRule? defaultRule = options.Rules.FirstOrDefault(rule => rule.ProviderName
+                == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
+
+            if (defaultRule is not null)
+            {
+                options.Rules.Remove(defaultRule);
+            }
+        });
     })
     .Build();
 
