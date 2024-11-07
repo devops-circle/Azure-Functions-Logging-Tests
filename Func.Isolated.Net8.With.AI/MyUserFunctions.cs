3d23008b764772e53178abde0c5c75347b5657fb
Ralph Jansen <ralph.jansen@outlook.com>
2024-11-07 14:25:03 +0100

    Create new .net 8 azure functions isolated project and copy over the test functions for boilerplate. No configuration is set yet. Just default project

---
+++ Func.Isolated.Net8.With.AI/MyUserFunctions.cs
@@ -0,0 +1,43 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Func.Isolated.Net8.With.AI
+{
+    public class MyUserFunctions
+    {
+        private readonly ILogger _logger;
+        private readonly IUserDataService _userDataService;
+
+        public MyUserFunctions(ILoggerFactory loggerFactory, IUserDataService userDataService)
+        {
+            _logger = loggerFactory.CreateLogger<MyUserFunctions>();
+            _userDataService = userDataService ?? throw new ArgumentNullException(nameof(userDataService));
+        }
+
+        [Function(nameof(GetUsers))]
+        public async Task<HttpResponseData> GetUsers([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        {
+            DateTime date = DateTime.UtcNow;
+
+            _logger.LogInformation("Some logging tests right now:");
+            _logger.LogTrace("Custom message as Trace " + date.ToLongTimeString());
+            _logger.LogDebug("Custom message as Debug " + date.ToLongTimeString());
+            _logger.LogInformation("Custom message as Information " + date.ToLongTimeString());
+            _logger.LogWarning("Custom message as Warning " + date.ToLongTimeString());
+            _logger.LogError("Custom message as Error " + date.ToLongTimeString());
+            _logger.LogCritical("Custom message as Critical " + date.ToLongTimeString());
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+
+            var users = await _userDataService.GetUsersAsync();
+            _logger.LogDebug("Number of users: " + users.Count);
+
+            // Set again statuscode because of issue: https://github.com/Azure/azure-functions-dotnet-worker/issues/776#issuecomment-1015633552
+            await response.WriteAsJsonAsync(users, response.StatusCode);
+
+            return response;
+        }
+    }
+}
---
+++ Func.Isolated.Net8.With.AI/UserDataService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Func.Isolated.Net8.With.AI;
+public interface IUserDataService
+{
+    Task<List<User>> GetUsersAsync();
+}
+
+public class UserDataService : IUserDataService
+{
+    private readonly ILogger<UserDataService> _logger;
+
+    public UserDataService(ILogger<UserDataService> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task<List<User>> GetUsersAsync()
+    {
+        _logger.LogTrace("GetUsersAsync called at: " + DateTime.Now.ToLongTimeString());
+        List<User> users = new List<User>
+        {
+            new User() { Id = "1", Name = "John", Age = 41 },
+            new User() { Id = "2", Name = "Jane", Age = 35 },
+            new User() { Id = "3", Name = "Piet", Age = 29 }
+        };
+
+        // Sleep so we mock database querying...
+        Thread.Sleep(1000);
+
+        _logger.LogTrace("GetUsersAsync finished at: " + DateTime.Now.ToLongTimeString());
+
+        return Task.FromResult(users);
+    }
+}
+
+public class User
+{
+    public string Id { get; set; } = default!;
+    public string Name { get; set; } = default!;
+    public int Age { get; set; }
+}
